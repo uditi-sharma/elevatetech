83f90c6fefac0c995ddda4b0de827737ade8220c
Sumathi Marappan <sumathi.marappan@kp.org>
2021-05-19 18:50:31 -0700

    [adding slideshow]

--- js/elevatehacks.js
+++ js/elevatehacks.js
@@ -74,3 +74,19 @@ x = setInterval(function () {
     document.getElementById("countdown-minutes").innerText = Math.floor((time % (hour)) / (minute));
     document.getElementById("countdown-seconds").innerText = Math.floor((time % (minute)) / second);
 }, 1000);
+
+//image slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000);
+}
